4b694c30025c672fcb09467712aaad467de00594
Arkadiusz Neuman <mrmoro@o2.pl>
2011-01-15 13:43:31 +0100

    debug
    Signed-off-by: Arkadiusz Neuman <mrmoro@o2.pl>

--- BNT/Form1.cs
+++ BNT/Form1.cs
@@ -19,25 +19,42 @@ namespace BNT
 
         private void button1_Click(object sender, EventArgs e)
         {
-            SqlCeConnection cn = new SqlCeConnection("Data Source=Baza_BNT.sdf;");
+            /*SqlCeConnection cn = new SqlCeConnection("Data Source=Baza_BNT.sdf;");
             SqlCeCommand cmd;
             SqlCeDataReader rdr = null;
 
             cn.Open();
             cmd = new SqlCeCommand("INSERT INTO Firmy (imie,nazwisko) VALUES ('Mietek','Szczesniak')", cn);
-
-            /*
+            //cmd = new SqlCeCommand("SELECT * FROM Firmy", cn);
+            
             rdr = cmd.ExecuteReader();
 
             while (rdr.Read())
             {
-                Console.WriteLine(rdr[0]);
+                MessageBox.Show(rdr[0].ToString()+" "+rdr[1].ToString()+" "+rdr[2].ToString());
             }
-            */
+            
 
             //cmd.ExecuteNonQuery();
             cmd.Dispose();
-            cn.Close();
+            cn.Close();*/
+
+            string conString = Properties.Settings.Default.Baza_BNTConnectionString;
+
+            // Open the connection using the connection string.
+            using (SqlCeConnection con = new SqlCeConnection(conString))
+            {
+                con.Open();
+
+                // Insert into the SqlCe table. ExecuteNonQuery is best for inserts.
+
+                using (SqlCeCommand com = new SqlCeCommand("INSERT INTO Firmy (imie,nazwisko) VALUES ('Mietekkkk','Szczesniak')", con))
+                {
+                    //com.Parameters.AddWithValue("@num", num);
+                    com.ExecuteNonQuery();
+                    
+                }
+            }
         }
     }
 }
