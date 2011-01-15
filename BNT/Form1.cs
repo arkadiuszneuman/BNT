83246c7495bc019ffa21d9aa727ffc96b46a720f
unknown <D:\The Bat!>
2011-01-15 13:02:43 +0100

    Jeszcze raz :P
    Signed-off-by: unknown <D:\The Bat!>

---
+++ BNT/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlServerCe;
+
+namespace BNT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlCeConnection cn = new SqlCeConnection("Data Source=Baza_BNT.sdf;");
+            SqlCeCommand cmd;
+            SqlCeDataReader rdr = null;
+
+            cn.Open();
+            cmd = new SqlCeCommand("INSERT INTO Firmy (imie,nazwisko) VALUES ('Mietek','Szczesniak')", cn);
+
+            /*
+            rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                Console.WriteLine(rdr[0]);
+            }
+            */
+
+            //cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            cn.Close();
+        }
+    }
+}
